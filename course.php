7ceece5bcb0157b904e1eec413e3624d7a47d438
Mimu Fujiwara <mimu.fujiwara@prologue.website>
2024-05-21 08:14:52 +0900

    ソース保存のため、最新データをダウンロード

---
+++ course.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <!-- 基本の共通head -->
+    <!-- <meta name="robots" content="noindex,nofollow">
+    <meta property="og:site_name" content="">
+    <meta property="og:image" content="">
+    <meta name="format-detection" content="telephone=no"> -->
+    <!-- 基本の共通head -->
+    <?php include "include/head.html" ?>
+    <!-- favicon、apple-touch-iconについては管理画面から設定 -->
+    <link rel="stylesheet" href="css/common/reset.css">
+    <link rel="stylesheet" href="css/common/common.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/course.css">
+</head>
+
+<body class="wrap">
+    <?php include "include/header.html" ?>
+    <div class="content">
+        <div class="pageHeader">
+            <div class="pageHeaderImg">
+                <img src="https://haruzushi.com/system_panel/uploads/images/20240430162051842657.png" alt="背景用の画像"
+                    width="1440" height="325" class="img" />
+            </div>
+            <div class="pageHeaderHead">
+                <h1 class="pageHeaderHeadTxt">コース
+                </h1>
+            </div>
+        </div>
+        <div class="decoration">
+            <img src="https://haruzushi.com/system_panel/uploads/images/20240426130859208013.png" alt="装飾用のイメージ画像"
+                width="232" height="304" class="decorationImg" />
+            <img src="https://haruzushi.com/system_panel/uploads/images/20240426130859292604.png" alt="装飾用のイメージ画像"
+                width="232" height="240" class="decorationImg" />
+            <div class="section courseDetail">
+                <div class="bottomLineTtl">
+                    <h2 class="h1714461713043">にぎり寿しご膳
+                    </h2>
+                    <p class="price">　5,500円
+                    </p>
+                </div>
+                <div class="courseDetailImg">
+                    <img src="https://haruzushi.com/system_panel/uploads/images/20240517153955403393.png"
+                        alt="にぎり寿しご膳 コース料理イメージ画像" width="968" height="646" class="img" />
+                </div>
+            </div>
+            <div class="section courseDetail">
+                <div class="bottomLineTtl">
+                    <h2 class="h1714461713084">にぎり寿しご膳
+                    </h2>
+                    <p class="price">6,600円
+                    </p>
+                </div>
+                <div class="courseDetailImg">
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include "include/footer.html" ?>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="js/script.js"></script>
+</body>
+
+</html>
